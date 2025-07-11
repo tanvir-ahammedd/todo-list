6488a0c8c997434e49a231e40512626f17068b31
Tanvir Ahammed <tanvircolorful@gmail.com>
2025-07-11 21:38:36 +0600

    Login Logout

--- base/views.py
+++ base/views.py
@@ -4,6 +4,15 @@ from django.views.generic import DetailView
 from django.views.generic import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Task
+from django.contrib.auth.views import LoginView
+
+class CustomLoginView(LoginView):
+    template_name = 'base/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('tasks')
 
 class TaskList(ListView):
     model = Task
