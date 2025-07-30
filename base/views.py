93c058f5c1053712c79e67ed04120cb915b53486
Tanvir Ahammed <tanvircolorful@gmail.com>
2025-07-30 09:21:28 +0600

    User Registration

--- base/views.py
+++ base/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from django.views.generic import DetailView
-from django.views.generic import CreateView, UpdateView, DeleteView
+from django.views.generic import CreateView, UpdateView, DeleteView, FormView
 from django.urls import reverse_lazy
+from django.shortcuts import redirect
 from .models import Task
 from .forms import TaskForm
+
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 class CustomLoginView(LoginView):
     template_name = 'base/login.html'
@@ -15,6 +19,26 @@ class CustomLoginView(LoginView):
 
     def get_success_url(self):
         return reverse_lazy('tasks')
+    
+class RegisterPage(FormView):
+    form_class = UserCreationForm
+    template_name = "base/register.html"
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('tasks')
+    
+    def form_valid(self, form):
+        user = form.save()
+        
+        # dont need to login after register, automatically login
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
+    
+    # login user cant go to register page, will redirect to tasks
+    def get(self, args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('tasks')
+    
 
 class TaskList(LoginRequiredMixin, ListView):
     model = Task
@@ -31,17 +55,6 @@ class TaskDetail(LoginRequiredMixin, DetailView):
     template_name = "base/details.html"
     context_object_name = "task"
 
-#option 1
-# class TaskCreate(LoginRequiredMixin, CreateView):
-#     model = Task
-#     fields = ['title', 'description', 'complete']
-#     success_url = reverse_lazy('tasks')
-    
-#     def form_valid(self, form):
-#         form.instance.user = self.request.user
-#         return super().form_valid(form)
-    
-# option 2
 class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
     form_class = TaskForm
@@ -49,7 +62,7 @@ class TaskCreate(LoginRequiredMixin, CreateView):
 
     def form_valid(self, form):
         form.instance.user = self.request.user
-        return super().form_valid(form)
+        return super(TaskCreate, self).form_valid(form)
 
 
 class TaskUpdate(LoginRequiredMixin, UpdateView):
